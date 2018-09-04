365056314b129b58b177d6eb4a1445bbae0d3aa6
debora torres guevara <deboratorresg@gmail.com>
2018-09-04 18:41:36 -0500

    cambios4

--- proyecto/src/proyecto/Proyecto.java
+++ proyecto/src/proyecto/Proyecto.java
@@ -17,6 +17,7 @@ public class Proyecto {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Holaaaaaaa");
+        System.out.println("¡Hola Osvaldo!");
     }
     
 }

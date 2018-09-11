2e92b70a13f428d6c82f36ad95b69ea254b5269c
debora torres guevara <deboratorresg@gmail.com>
2018-09-11 18:02:43 -0500

    cambios4

--- proyecto2/src/proyecto2/Proyecto2.java
+++ proyecto2/src/proyecto2/Proyecto2.java
@@ -16,7 +16,7 @@ public class Proyecto2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("¡Hola!!");
+        System.out.println("¿Cómo están?");
     }
     
 }
